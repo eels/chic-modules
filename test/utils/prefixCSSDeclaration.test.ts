94831ed0fd7fd9019498a0a1996ae429447f986d
Liam Howell <liam@liam.codes>
2021-10-17 21:25:17 +0100

    feat: add test for prefix css rule utility

---
+++ test/utils/prefixCSSDeclaration.test.ts
@@ -0,0 +1,27 @@
+import prefixCSSDeclaration from '../../src/utils/prefixCSSDeclaration';
+
+describe('prefixCSSDeclaration', () => {
+  it('should return a basic constructed css rule', () => {
+    const prefixedRule = prefixCSSDeclaration('background-color', 'black');
+
+    expect(prefixedRule).toBe('background-color:black;');
+  });
+
+  it('should return a css rule with an alias property when required', () => {
+    const prefixedRule = prefixCSSDeclaration('gap', '10px');
+
+    expect(prefixedRule).toBe('grid-gap:10px;gap:10px;');
+  });
+
+  it('should return a css rule with a prefixed property when required', () => {
+    const prefixedRule = prefixCSSDeclaration('backdrop-filter', 'none');
+
+    expect(prefixedRule).toBe('-webkit-backdrop-filter:none;backdrop-filter:none;');
+  });
+
+  it('should return a css rule with a prefixed value when required', () => {
+    const prefixedRule = prefixCSSDeclaration('position', 'sticky');
+
+    expect(prefixedRule).toBe('position:-webkit-sticky;position:sticky;');
+  });
+});
