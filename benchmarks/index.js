2ac8e937367318e0c60c132915215ac215b558e9
Liam Howell <liam@liam.codes>
2021-10-24 22:20:02 +0100

    fix: fix benchmarks

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,12 +1,12 @@
-import createCanary from '../dist/chic-modules.cjs';
-import createLatest from '../node_modules/chic-modules/dist/chic-modules.cjs';
+import Benchmark from 'benchmark';
+import React from 'react';
 import fs from 'fs';
-import { Suite } from 'benchmark';
-import { createElement } from 'react';
+import { create as createCanary } from '../dist/chic-modules.cjs';
+import { create as createLatest } from '../node_modules/chic-modules/dist/chic-modules.cjs';
 import { renderToString } from 'react-dom/server.js';
 
 function renderComponent(Component) {
-  const rendered = createElement(Component, {
+  const rendered = React.createElement(Component, {
     hasBorder: true,
     hasInvalidModifier: true,
     id: 'heading',
@@ -29,7 +29,7 @@ function onBenchmarkCycle(event) {
   console.log('▸', String(event.target));
 }
 
-const suite = new Suite('chic-modules');
+const suite = new Benchmark.Suite('chic-modules');
 const styles = JSON.parse(fs.readFileSync('./test/__mocks__/styles.module.json'));
 const styledCanary = createCanary(styles);
 const styledLatest = createLatest(styles);
