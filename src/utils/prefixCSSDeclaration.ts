bfbb18dfe384be278648f822d9db62dea988e97d
Liam Howell <liam@liam.codes>
2021-10-25 20:46:07 +0100

    refactor: rewrite prefix function

--- src/utils/prefixCSSDeclaration.ts
+++ src/utils/prefixCSSDeclaration.ts
@@ -2,23 +2,25 @@ import { cssPropertyAlias, cssPropertyPrefixFlags, cssValuePrefixFlags } from 's
 
 export default function prefixCSSDeclaration(property: string, value: any) {
   const output = <string[]>[];
+  const vendorFlags = { 0b001: '-webkit-', 0b010: '-moz-', 0b100: '-ms-' };
+  const vendorFlagEntries = Object.entries(vendorFlags);
+
   const hasPropertyAlias = cssPropertyAlias(property);
   const hasPropertyFlags = cssPropertyPrefixFlags(property);
-  const hasValueFlags = cssValuePrefixFlags(property, value);
-  const vendorFlags = { 0b001: '-webkit-', 0b010: '-moz-', 0b100: '-ms-' };
+  const hasCSSValueFlags = cssValuePrefixFlags(property, value);
 
   if (hasPropertyAlias) {
     output.push(`${hasPropertyAlias}:${value};`);
   }
 
-  for (const [bit, vendor] of Object.entries(vendorFlags)) {
+  for (const [bit, vendor] of vendorFlagEntries) {
     const bitnum = parseInt(bit);
 
     if (hasPropertyFlags & bitnum) {
       output.push(`${vendor}${property}:${value};`);
     }
 
-    if (hasValueFlags & bitnum) {
+    if (hasCSSValueFlags & bitnum) {
       output.push(`${property}:${vendor}${value};`);
     }
   }
