b5f74c379b3146ee914927f502091dd9ee557f34
Liam Howell <liam@liam.codes>
2021-10-25 20:57:30 +0100

    refactor: remove unnecessary conditional in hash function

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -2,10 +2,6 @@ export default function hash(input: string) {
   let hash = 0;
   let i = input.length;
 
-  if (i === 0) {
-    return hash;
-  }
-
   while (i) {
     hash = (hash << 5) - hash + input.charCodeAt(--i);
     hash |= 0;
