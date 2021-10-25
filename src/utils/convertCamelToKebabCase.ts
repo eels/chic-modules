e5034476df4e0fa8dbcc4d4b6f3a29155d459429
Liam Howell <liam@liam.codes>
2021-10-25 20:44:31 +0100

    refactor: move replacer function inline

--- src/utils/convertCamelToKebabCase.ts
+++ src/utils/convertCamelToKebabCase.ts
@@ -1,7 +1,5 @@
 export default function convertCamelToKebabCase(string: string) {
-  function replacer(letter: string, offset: number) {
+  return string.replace(/[A-Z]/g, (letter: string, offset: number) => {
     return `${offset !== 0 ? '-' : ''}${letter.toLowerCase()}`;
-  }
-
-  return string.replace(/[A-Z]/g, replacer);
+  });
 }
