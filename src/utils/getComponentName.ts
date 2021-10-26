9beabb521ee2296e55ad79e57be244b9551feaae
Liam Howell <liam@liam.codes>
2021-10-26 23:05:42 +0100

    refactor: streamline display name generation function

--- src/utils/getComponentName.ts
+++
@@ -1,7 +0,0 @@
-import type { ChicTarget } from '@types';
-
-export default function getComponentName(target: ChicTarget) {
-  return (
-    (<Exclude<ChicTarget<any>, string>>target).displayName || (<Function>target).name || 'Component'
-  );
-}
