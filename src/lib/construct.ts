0e6e9e96fe745dbb227e13860f5937d86b5cebde
Liam Howell <liam@liam.codes>
2021-10-26 18:55:09 +0100

    refactor: rewrite construct function code to reduce loops

--- src/lib/construct.ts
+++ src/lib/construct.ts
@@ -1,5 +1,4 @@
-import cc from 'classcat';
-import circularStringify from '@src/utils/circularStringify';
+import cc, { Class } from 'classcat';
 import convertCamelToKebabCase from '@src/utils/convertCamelToKebabCase';
 import generateDisplayName from '@src/utils/generateDisplayName';
 import hash from '@src/utils/hash';
@@ -18,91 +17,74 @@ export default function construct<Props = ChicProps>(options: ConstructOptions<P
   const isSingularClassName = isType(classNames, 'string');
   const isTargetObject = isType(target, 'object');
   const classNamesArray = <string[]>(!isSingularClassName ? classNames : [classNames]);
+  const classNamesHash = hash(classNamesArray.join(''));
 
   function wrapper() {
-    const cache: ExtendableObject = {};
     const name = generateDisplayName(<any>target);
 
     function styled(props: Props, ref: Ref<Element>) {
       const constructedProps = <ChicProps>Object.assign({}, attrs, props);
-      const constructedPropsHash = hash(circularStringify(constructedProps));
-      const constructedPropsKeys = Object.keys(constructedProps);
+      const constructedPropsEntries = Object.entries(constructedProps);
       const as = constructedProps.as || target;
       const hasValidAs = isType(as, ['function', 'object', 'string']);
       const element = hasValidAs && !isTargetObject ? as : target;
+      const componentClassNames = <Class[]>[];
+      const dynamicStyleClassNames = <string[]>[];
+      const modifiers: ExtendableObject<Props[Extract<keyof Props, string>]> = {};
 
-      if (!cache[constructedPropsHash]) {
+      if (constructedPropsEntries.length === 0) {
+        constructedPropsEntries.push([`__chic-${Date.now().toString(36)}`, false]);
+      }
+
+      for (const [prop, value] of constructedPropsEntries) {
         for (const className of classNamesArray) {
-          if (!styles[className]) {
+          const stylesLookup = styles[className];
+
+          if (!stylesLookup) {
             throw new Error(`Target class "${className}" does not exist in provided module`);
           }
 
-          const stylesLookup = styles[className];
-          const modifiers: ExtendableObject<Props[Extract<keyof Props, string>]> = {};
+          componentClassNames.push(stylesLookup);
 
-          for (const prop of constructedPropsKeys) {
-            const propValue = constructedProps[prop];
-
-            if (!prefixes.some((prefix) => !!prop.match(`^${prefix}`)) || !propValue) {
-              continue;
-            }
-
+          if (prefixes.some((prefix) => !!prop.match(`^${prefix}`))) {
             const prefix = prop.match(prefixesRegex)?.[0];
             const modifier = convertCamelToKebabCase(prop.replace(prefixesRegex, ''));
-
             const baseClassName = `${className}--${modifier.toLowerCase()}`;
-            const modifierValueExtention = prefix === 'with' ? `-${propValue}` : '';
+            const modifierValueExtention = prefix === 'with' ? `-${value}` : '';
             const constructedClassName = `${baseClassName}${modifierValueExtention}`;
             const constructedStylesLookup = styles[constructedClassName];
 
             if (constructedStylesLookup) {
-              modifiers[constructedStylesLookup] = propValue;
+              modifiers[constructedStylesLookup] = value;
             }
-          }
-
-          constructedProps.className = cc([stylesLookup, modifiers, constructedProps.className]);
-        }
-
-        cache[constructedPropsHash] = constructedProps.className;
-      }
-
-      constructedProps.className = cache[constructedPropsHash];
-
-      if (!isTargetObject) {
-        for (const prop of constructedPropsKeys) {
-          if (!isValidProp(prop)) {
-            delete constructedProps[prop];
           }
         }
 
-        if (constructedProps.style && isType(constructedProps.style, 'object')) {
-          const classNamesHash = hash(classNamesArray.join(''));
-          const stylesHash = hash(JSON.stringify(constructedProps.style));
-          const combinedClassNameStyleHash = hash(`${classNamesHash}${stylesHash}`).toString(36);
+        if (!isTargetObject && !isValidProp(prop)) {
+          delete constructedProps[prop];
+        }
 
-          if (!cache[combinedClassNameStyleHash]) {
-            const stylesClassName = `cm${combinedClassNameStyleHash}`;
-            const styles = <string[]>[];
-
-            constructedProps.className = cc([constructedProps.className, stylesClassName]);
-
-            for (const [property, value] of Object.entries(constructedProps.style)) {
-              const kebabCasedProperty = convertCamelToKebabCase(property);
-              const rule = prefixCSSDeclaration(kebabCasedProperty, value);
-
-              styles.push(rule);
-            }
-
-            insertDynamicCSSRule(stylesClassName, styles);
-
-            cache[combinedClassNameStyleHash] = constructedProps.className;
-          }
+        if (!isTargetObject && prop === 'style' && isType(value, 'object')) {
+          const styleHash = hash(JSON.stringify(value));
+          const constructedStyleHash = hash(`${classNamesHash}${styleHash}`).toString(36);
+          const styleEntries = Object.entries(value);
+          const styleClassName = `cm${constructedStyleHash}`;
+          const dynamicStyles = <string[]>[];
 
           delete constructedProps.style;
+          dynamicStyleClassNames.push(styleClassName);
 
-          constructedProps.className = cache[combinedClassNameStyleHash];
+          for (const [property, value] of styleEntries) {
+            dynamicStyles.push(prefixCSSDeclaration(convertCamelToKebabCase(property), value));
+          }
+
+          insertDynamicCSSRule(styleClassName, dynamicStyles);
         }
       }
+
+      componentClassNames.push(modifiers, dynamicStyleClassNames, constructedProps.className);
+
+      constructedProps.className = cc(componentClassNames);
 
       const propsRefObject = ref ? { ref } : {};
       const propsToForward = Object.assign({}, constructedProps, propsRefObject);
